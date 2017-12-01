e20b25378d80fe218077b21076627bcf12dbe0ff
Ellie Foreman <Ellie@Ellies-MacBook-Air.local>
2017-12-01 10:18:50 +0000

    initial commit of end of week quiz folder

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h3>End of week test </h3>
+    );
+  }
+}
+
+export default App;
